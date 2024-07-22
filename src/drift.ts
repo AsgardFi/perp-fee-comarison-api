c3719d662c66b014e166d5ea8290fb9abb10bc9d
Deep Mehta <65382963+0xdeepmehta@users.noreply.github.com>
2024-07-22 19:33:17 +0530

    chore: comented drift things

--- src/drift.ts
+++ src/drift.ts
@@ -6,47 +6,47 @@ import * as driftIDL from './drift_idl.json';
 
 export const driftEntryPoint = async (connection: Connection) => {
 
-    // SOL-PERP
-    const SOL_PERP = new PublicKey("8UJgxaiQx5nTrdDgph5FiahMmzduuLTLf5WmsPegYA6W")
+    // // SOL-PERP
+    // const SOL_PERP = new PublicKey("8UJgxaiQx5nTrdDgph5FiahMmzduuLTLf5WmsPegYA6W")
 
-    // // 1MBONK-PERP
-    // const BONK_PERP = new PublicKey("2QeqpeJUVo2LBWNELRfcBwJgrNoxJQSd7gokcaM5nvaa")
+    // // // 1MBONK-PERP
+    // // const BONK_PERP = new PublicKey("2QeqpeJUVo2LBWNELRfcBwJgrNoxJQSd7gokcaM5nvaa")
     
-    const borshDecoder = new BorshAccountsCoder(driftIDL as Idl)
+    // const borshDecoder = new BorshAccountsCoder(driftIDL as Idl)
 
-    const solPerpRawAccount = await connection.getAccountInfo(SOL_PERP)
-    // const bonkPerpRawAccount = await connection.getAccountInfo(BONK_PERP)
+    // const solPerpRawAccount = await connection.getAccountInfo(SOL_PERP)
+    // // const bonkPerpRawAccount = await connection.getAccountInfo(BONK_PERP)
 
 
-    if (!solPerpRawAccount) throw Error("solPerpRawAccount not found")
-    // if (!bonkPerpRawAccount) throw Error("bonkPerpRawAccount not found")
+    // if (!solPerpRawAccount) throw Error("solPerpRawAccount not found")
+    // // if (!bonkPerpRawAccount) throw Error("bonkPerpRawAccount not found")
 
-    const solPerpMarketAccount = borshDecoder.decode("PerpMarket", solPerpRawAccount.data) as PerpMarketAccount;
-    // const bonkPerpMarketAccount = borshDecoder.decode("PerpMarket", bonkPerpRawAccount.data) as PerpMarketAccount;
+    // const solPerpMarketAccount = borshDecoder.decode("PerpMarket", solPerpRawAccount.data) as PerpMarketAccount;
+    // // const bonkPerpMarketAccount = borshDecoder.decode("PerpMarket", bonkPerpRawAccount.data) as PerpMarketAccount;
 
-    const pythClient = new PythClient(connection)
+    // const pythClient = new PythClient(connection)
 
-    async function getPerpMarketData(perpMarketAccount: PerpMarketAccount) {
-        const oracleKey = perpMarketAccount.amm.oracle
-        const rawOracleAccount = await connection.getAccountInfo(oracleKey);
-        if (!rawOracleAccount) throw Error("rawOracleAccount not found");
+    // async function getPerpMarketData(perpMarketAccount: PerpMarketAccount) {
+    //     const oracleKey = perpMarketAccount.amm.oracle
+    //     const rawOracleAccount = await connection.getAccountInfo(oracleKey);
+    //     if (!rawOracleAccount) throw Error("rawOracleAccount not found");
 
-        const oraclePriceData = pythClient.getOraclePriceDataFromBuffer(rawOracleAccount.data)
-        const fundingRate = await calculateLongShortFundingRate(perpMarketAccount, oraclePriceData)
+    //     const oraclePriceData = pythClient.getOraclePriceDataFromBuffer(rawOracleAccount.data)
+    //     const fundingRate = await calculateLongShortFundingRate(perpMarketAccount, oraclePriceData)
 
-        return {
-            longFundingRate: fundingRate[0].toNumber() / 1e6,
-        }
-    }
+    //     return {
+    //         longFundingRate: fundingRate[0].toNumber() / 1e6,
+    //     }
+    // }
 
-    const solPerpData = await getPerpMarketData(solPerpMarketAccount)
-    // const bonkPerpData = await getPerpMarketData(bonkPerpMarketAccount)
+    // const solPerpData = await getPerpMarketData(solPerpMarketAccount)
+    // // const bonkPerpData = await getPerpMarketData(bonkPerpMarketAccount)
 
-    const driftPerpMetadata = {
-        SOLHourlyFundingRate: solPerpData.longFundingRate,
-        // BONK: bonkPerpData.longFundingRate
-    }
+    // const driftPerpMetadata = {
+    //     SOLHourlyFundingRate: solPerpData.longFundingRate,
+    //     // BONK: bonkPerpData.longFundingRate
+    // }
 
-    return driftPerpMetadata
+    // return driftPerpMetadata
 
 }
